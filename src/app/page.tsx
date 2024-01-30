1f916f9e727155317a0ac1e543a8e493edd5abfa
Kristoffer <krisolberg@gmail.com>
2024-01-30 11:31:44 +0100

    move page to app directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+"use client";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
-import { type NeedleEngineProps } from '../needleEngine'
-const NeedleEngine = dynamic<NeedleEngineProps>(() => import('../needleEngine'), { ssr: false })
+import { type NeedleEngineProps } from "../needleEngine";
+const NeedleEngine = dynamic<NeedleEngineProps>(
+  () => import("../needleEngine"),
+  { ssr: false }
+);
 
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-
     <main className="flex min-h-screen flex-col items-center justify-between min-[800px]:p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -26,7 +27,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -40,12 +41,11 @@ export default function Home() {
       </div>
 
       <div className="relative flex items-center justify-between w-full h-[48rem] lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 my-5">
-
         <NeedleEngine
           loading-style="light"
           style={{
-            width: '100%',
-            height: '100%',
+            width: "100%",
+            height: "100%",
           }}
         />
       </div>
@@ -58,7 +58,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -77,7 +77,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -96,7 +96,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -115,7 +115,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -128,5 +128,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
